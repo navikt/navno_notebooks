cfa1d7367aaa5dce1d7bef30fd00f24ff4d8356d
Jørgen Nordmoen <jorgen@nordmoen.net>
2024-11-06 15:41:09 +0100

    Støtte for første macro håndtering

--- src/navno_notebooks/parser/html.py
+++ src/navno_notebooks/parser/html.py
@@ -2,9 +2,19 @@
 
 from typing import Any
 
+from bs4 import BeautifulSoup
+
 
 def parse_area(content: dict[str, Any]) -> str:
     """Parser for type 'no.nav.navno:html-area'."""
     # TODO: Parse 'data macro' her
     html: str = content["config"]["html"]["processedHtml"]
-    return html
+    soup = BeautifulSoup(html, "lxml")
+    editor_macros = soup.find_all("editor-macro")
+    for macro in editor_macros:
+        name = macro.attrs["data-macro-name"]
+        if name == "product-card-mini":
+            macro.extract()
+        else:
+            raise NotImplementedError(f"Mangler støtte for macro av type: {name}")
+    return str(soup)
