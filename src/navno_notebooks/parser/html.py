2c32440c1d2a62748b2f50b6e41c6691b20af910
Jørgen Nordmoen <jorgen@nordmoen.net>
2024-11-06 16:28:59 +0100

    Bruk macro funksjonalitet

--- src/navno_notebooks/parser/html.py
+++ src/navno_notebooks/parser/html.py
@@ -4,17 +4,21 @@ from typing import Any
 
 from bs4 import BeautifulSoup
 
+from .macros import parse_macros
+
 
 def parse_area(content: dict[str, Any]) -> str:
     """Parser for type 'no.nav.navno:html-area'."""
     # TODO: Parse 'data macro' her
     html: str = content["config"]["html"]["processedHtml"]
-    soup = BeautifulSoup(html, "lxml")
-    editor_macros = soup.find_all("editor-macro")
-    for macro in editor_macros:
-        name = macro.attrs["data-macro-name"]
-        if name == "product-card-mini":
-            macro.extract()
-        else:
-            raise NotImplementedError(f"Mangler støtte for macro av type: {name}")
-    return str(soup)
+    if macros := parse_macros(content["config"]["html"]["macros"]):
+        soup = BeautifulSoup(html, "lxml")
+        editor_macros = soup.find_all("editor-macro")
+        for macro in editor_macros:
+            ref = macro.attrs["data-macro-ref"]
+            if replacement := macros.get(ref):
+                macro.replace_with(replacement)
+            else:
+                macro.extract()
+        return str(soup)
+    return html
